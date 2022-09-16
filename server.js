f333489c70caf079a1e6be89a412ea5ed1f6170f
ABU RAGHIB UMAR <raghibumar2@gmail.com>
2022-09-16 06:08:25 +0530

    make

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("./data/db.json");
+// const middlewares = jsonServer.defaults({
+//   static: "./build",
+// });
+
+// const port = process.env.PORT || 8080;
+// server.use(middlewares);
+// server.use(
+//   jsonServer.rewriter({
+//     "/http://localhost:8080/blogs/*": "/$1",
+//   })
+// );
+
+// server.use(router);
+// server.listen(port, () => {
+//   console.log(`server is running on ${port}`);
+// });
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./data/db.json");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
-
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
-    "/http://localhost:8080/blogs/*": "/$1",
+    "/api/*": "/$1",
   })
 );
-
 server.use(router);
-server.listen(port, () => {
-  console.log(`server is running on ${port}`);
+server.listen(PORT, () => {
+  console.log("Server is running");
 });
